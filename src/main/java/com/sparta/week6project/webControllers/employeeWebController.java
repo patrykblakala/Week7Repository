cda515dfeb4ad1ee11b99663d1c3ab3e144ca880
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2023-01-12 10:03:09 +0000

    removed unused imports and formatted code.

--- src/main/java/com/sparta/week6project/webControllers/employeeWebController.java
+++ src/main/java/com/sparta/week6project/webControllers/employeeWebController.java
@@ -1,7 +1,6 @@
 package com.sparta.week6project.webControllers;
 
 import com.sparta.week6project.DAO.impl.EmployeeDAO;
-import com.sparta.week6project.DTO.DepartmentDTO;
 import com.sparta.week6project.DTO.EmployeeDTO;
 import com.sparta.week6project.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +18,7 @@ public class employeeWebController {
     EmployeeDAO employeeDAO;
 
     @GetMapping("/all/{pageNum}")
-    public String getAllEmployees(Model model,@PathVariable int pageNum){
+    public String getAllEmployees(Model model, @PathVariable int pageNum) {
         Page<Employee> employees = employeeDAO.findAllEmployees(pageNum);
         model.addAttribute("employees", employees);
         model.addAttribute("pageNum", pageNum);
@@ -27,52 +26,52 @@ public class employeeWebController {
     }
 
     @GetMapping("/{id}")
-    public String getEmployeeById(Model model, @PathVariable int id){
+    public String getEmployeeById(Model model, @PathVariable int id) {
         EmployeeDTO employeeDTO = employeeDAO.findById(id).orElse(null);
-        model.addAttribute("employee",employeeDTO);
+        model.addAttribute("employee", employeeDTO);
         return "employee/displayEmployee";
     }
 
     @GetMapping("/")
-    public String getEmployeeByLastName(Model model, @RequestParam String lastName){
+    public String getEmployeeByLastName(Model model, @RequestParam String lastName) {
         List<Employee> employeeList = employeeDAO.findEmployeeByLastName(lastName);
-        model.addAttribute("employees",employeeList);
+        model.addAttribute("employees", employeeList);
         return "employee/displayAllEmployeesLastName";
     }
 
     @GetMapping("/createEmployee")
-    public String createEmployee(Model model){
+    public String createEmployee(Model model) {
         EmployeeDTO employeeDTO = new EmployeeDTO();
-        model.addAttribute("employee",employeeDTO);
+        model.addAttribute("employee", employeeDTO);
         return "employee/createEmployeePage";
     }
 
     @PostMapping("/createSuccess")
-    public String createDepartmentSuccess(@ModelAttribute("employee") EmployeeDTO employeeDTO, Model model){
+    public String createDepartmentSuccess(@ModelAttribute("employee") EmployeeDTO employeeDTO, Model model) {
         employeeDAO.save(employeeDTO);
         return "employee/createSuccessPage";
     }
 
     @GetMapping("/updateEmployee/{id}")
-    public String updateEmployee(Model model, @PathVariable int id){
+    public String updateEmployee(Model model, @PathVariable int id) {
         EmployeeDTO employeeDTO = employeeDAO.findById(id).orElse(null);
-        model.addAttribute("employee",employeeDTO);
+        model.addAttribute("employee", employeeDTO);
         return "employee/updateEmployeePage";
     }
 
     @PostMapping("/updateEmployee/updateSuccess")
-    public String updateEmployeeSuccess(@ModelAttribute("employee") EmployeeDTO employeeDTO, Model model){
+    public String updateEmployeeSuccess(@ModelAttribute("employee") EmployeeDTO employeeDTO, Model model) {
         employeeDAO.save(employeeDTO);
         return "employee/updateSuccessPage";
     }
 
     @GetMapping("/deleteEmployee/{id}")
-    public String deleteEmployee(@PathVariable int id, Model model){
+    public String deleteEmployee(@PathVariable int id, Model model) {
         EmployeeDTO employeeDTO = employeeDAO.findById(id).orElse(null);
-        if (employeeDTO != null){
+        if (employeeDTO != null) {
             employeeDAO.deleteById(id);
         }
-        model.addAttribute("employee",employeeDTO);
+        model.addAttribute("employee", employeeDTO);
         return "employee/deleteSuccessPage";
 
     }
