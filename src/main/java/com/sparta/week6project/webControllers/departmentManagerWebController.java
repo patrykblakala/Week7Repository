87a2ba69de3d9223acf16e82404103ca3d219866
OGrant <OmariGrant@users.noreply.github.com>
2023-01-13 12:04:42 +0000

    updated security and bootstrap

--- src/main/java/com/sparta/week6project/webControllers/departmentManagerWebController.java
+++ src/main/java/com/sparta/week6project/webControllers/departmentManagerWebController.java
@@ -2,23 +2,25 @@ package com.sparta.week6project.webControllers;
 
 import com.sparta.week6project.DAO.impl.DepartmentManagerDAO;
 import com.sparta.week6project.DTO.DeptManagerDTO;
-import com.sparta.week6project.DTO.SalaryDTO;
-import com.sparta.week6project.entities.*;
+import com.sparta.week6project.entities.DeptManager;
+import com.sparta.week6project.entities.DeptManagerId;
 import com.sparta.week6project.repositories.DeptManagerRepository;
-import com.sparta.week6project.repositories.SalaryRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
+@Log4j2
 @RequestMapping("/web/departmentManager")
+
 public class departmentManagerWebController {
     @Autowired
     DepartmentManagerDAO departmentManagerDAO;
@@ -26,20 +28,21 @@ public class departmentManagerWebController {
     @Autowired
     DeptManagerRepository deptManagerRepository;
 
-    @GetMapping("/all")
+
+    @GetMapping("/basic/all")
     public String getAllSalaries(Model model){
         List<DeptManager> departmentManagers = departmentManagerDAO.findAll();
         model.addAttribute("departmentManagers", departmentManagers);
         return "departmentManager/all";
     }
 
-    @GetMapping("/show")
+    @GetMapping("/basic/show")
     public String getSalaryById(Integer empNo, String deptNo, Model model){
 
         DeptManagerId deptManagerId = new DeptManagerId();
         deptManagerId.setEmpNo(empNo);
         deptManagerId.setDeptNo(deptNo);
-        
+
         Optional<DeptManagerDTO> deptManagerDTOOptional = departmentManagerDAO.findById(deptManagerId);
         DeptManagerDTO departmentManager = null;
         if(deptManagerDTOOptional.isPresent()){
@@ -49,7 +52,7 @@ public class departmentManagerWebController {
         return "departmentManager/display";
     }
 
-    @GetMapping("/update")
+    @GetMapping("/update/update")
     public String updateSalaryById(Integer empNo, String deptNo, Model model){
 
         DeptManagerId deptManagerId = new DeptManagerId();
@@ -65,14 +68,14 @@ public class departmentManagerWebController {
         return "departmentManager/update";
     }
 
-    @GetMapping("/create")
+    @GetMapping("/update/create")
     public String createSalary(Model model){
         DeptManagerDTO departmentManager = new DeptManagerDTO();
         model.addAttribute("departmentManager",departmentManager);
         return "departmentManager/create";
     }
 
-    @PostMapping("/success")
+    @PostMapping("/update/success")
     public String createDepartmentSuccess(@ModelAttribute("departmentManager")DeptManagerDTO departmentManager){
 
         deptManagerRepository.saveDeptManager(departmentManager.getEmpNo(), departmentManager.getDeptNo(), departmentManager.getFromDate(), departmentManager.getToDate());
@@ -80,7 +83,7 @@ public class departmentManagerWebController {
         return "departmentManager/success";
     }
 
-    @PostMapping("/updateSuccess")
+    @PostMapping("/update/updateSuccess")
     public String updateDepartmentSuccess(@ModelAttribute("departmentManager") DeptManagerDTO departmentManager){
 
         DeptManagerId deptManagerId = new DeptManagerId();
@@ -104,7 +107,7 @@ public class departmentManagerWebController {
         return "departmentManager/success";
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/admin/delete")
     public String delete(@ModelAttribute("departmentManager") DeptManagerDTO departmentManager){
 
         DeptManagerId deptManagerId = new DeptManagerId();
